b21771b1ff73f48b8be90f0c6946eb52269fe8c0
Emin Fedar <eminfedar@gmail.com>
2026-05-01 14:58:33 +0300

    fix: remove unused cli args

--- src/cli.rs
+++ src/cli.rs
@@ -1,20 +1,18 @@
-use clap::{ArgAction, Parser};
-use std::path::PathBuf;
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(name = "tusic", author, version, about, long_about)]
-#[command(version = "0.1.0")]
+#[command(version = "0.1.2")]
 #[command(about = "A terminal music player", long_about = None)]
 pub struct Args {
-    #[arg(short, long, help = "Path to scan for music files")]
-    pub path: Option<PathBuf>,
+    //#[arg(short, long, help = "Path to scan for music files")]
+    //pub path: Option<PathBuf>,
+    //#[arg(short, long, help = "Do not scan directories automatically")]
+    //pub no_scan: bool,
 
-    #[arg(short, long, help = "Do not scan directories automatically")]
-    pub no_scan: bool,
+    //#[arg(short, long, help = "Recursive scan depth (default: 10)")]
+    //pub depth: Option<usize>,
 
-    #[arg(short, long, help = "Recursive scan depth (default: 10)")]
-    pub depth: Option<usize>,
-
-    #[arg(short, long, action = ArgAction::SetTrue, help = "Verbose output")]
-    pub verbose: bool,
+    //#[arg(short, long, action = ArgAction::SetTrue, help = "Verbose output")]
+    //pub verbose: bool,
 }
